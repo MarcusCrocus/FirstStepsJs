506fb0b3978f18fc78d72d1a633a4994ba038f5e
Marcus <crocushtml@gmail.com>
2021-02-16 19:50:10 +0100

    3ed home task finnished

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 "use strict";
+/* //                                            #1
 
 /*1) Create a variable numberOfFilms and put the answer from the user to the question in it:
 'How many films have you watched?'  */
 
-let numberOfFilms = prompt('How many films have u watched?', '');
+/* const numberOfFilms = prompt('How many films have u watched?', ''); */
     
 
 /* 2) Create a personalMovieDB object and put the following properties in it:
@@ -14,13 +15,13 @@ let numberOfFilms = prompt('How many films have u watched?', '');
     - genres - put an empty array here
     - privat - put boolean (boolean) value false in this property */
 
-let personalMoviesDB = {
+/* const personalMoviesDB = {
     count: numberOfFilms, 
     movies: {},
     actors: {},
     genders: [],
     privat: false
-};
+}; */
 
 /* 3) Ask user each of questions twice:
     - 'One of the last movies you watched?'
@@ -31,14 +32,168 @@ Write responses to the movies object in the format:
         'logan': '8.1'
     } */
 
-let lastFilm = prompt('What was u last film?', ''),
+/*  const lastFilm = prompt('What was u last film?', ''),
     rate     = prompt('how do u rate it?', ''),
     b        = prompt('What was u last film?', ''),
     c        = prompt('how do u rate it?', '');
 
 personalMoviesDB.movies[lastFilm] = rate;
 personalMoviesDB.movies[b] = c;   //в квадратных во избежание багов с кирилицей
+                                 // не использовать через точку personalMoviesDB.movies.lastFilm = rate; 
 
-console.log(personalMoviesDB);
+console.log(personalMoviesDB);  */
+
+                                 // #2
+
+/* 1) Automate questions to the user about films using a loop */
+
+/*  for (let i = 0; i < 2; i++) {
+    const lastFilm = prompt('What was u last film?', ''),
+          rate     = prompt('how do u rate it?', '');
+
+    personalMoviesDB.movies[lastFilm] = rate;
+}
+
+console.log(personalMoviesDB);  */
 
 
+/* 2) Make it so that the user cannot leave a response as an empty line,
+cancel reply or movie title longer than 50 characters. If this happens -
+return the user to the questions again */
+
+/* for (let i = 0; i < 2; i++) {
+    let lastFilm = prompt('What was u last film?', ''),
+        rate     = prompt('how do u rate it?', '');
+
+    if (lastFilm != null && rate != null && lastFilm != '' && rate != '' && lastFilm.length < 50) {
+    personalMoviesDB.movies[lastFilm] = rate; 
+    console.log('done');
+    }else{
+        console.log('error');
+        i--;
+    }
+
+}
+ console.log(personalMoviesDB); */
+
+/* 3) Using conditions, check personalMovieDB.count, and if it is less than 10 - display a message
+"Quite a few films have been watched", if from 10 to 30 - "You are a classic spectator", and if more -
+"You are a movie fan." And if it did not fit any option - "An error occurred" */ 
+
+/* const numberOfFilms = prompt('How many films have u watched?', ''); */
+
+/* const personalMoviesDB = {
+        count: numberOfFilms, 
+        movies: {},
+        actors: {},
+        genders: [],
+        privat: false
+};
+ */
+
+/* for (let i = 0; i < 2; i++) {
+    let lastFilm = prompt('What was u last film?', ''),
+        rate     = prompt('how do u rate it?', '');
+
+    if (lastFilm != null && rate != null && lastFilm != '' && rate != '' && lastFilm.length < 50) {
+        personalMoviesDB.movies[lastFilm] = rate; 
+        console.log('done');
+    }else{
+        console.log('error');
+        i--;
+    }
+
+} */
+
+/* if (personalMoviesDB.count < 10) {
+    console.log('too moderate quantity of films');
+} else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30){
+    console.log ('You are a classic viewer');
+} else if (personalMoviesDB.count >=30) {
+    console.log ('U are a big fun of movies');
+} else{
+    console.log('error');
+}
+
+console.log(personalMoviesDB); */
+
+                        // #3
+
+let numberOfFilms;
+
+function start () {
+    numberOfFilms = +prompt('How many films have u watched?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many films have u watched?', '');
+    }
+}
+
+start();
+
+let personalMoviesDB = {
+    count: numberOfFilms, 
+    movies: {},
+    actors: {},
+    genders: [],
+    privat: false
+};
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let lastFilm = prompt('What was u last film?', ''),
+            rate     = prompt('how do u rate it?', '');
+    
+        if (lastFilm != null && rate != null && lastFilm != '' && rate != '' && lastFilm.length < 50) {
+            personalMoviesDB.movies[lastFilm] = rate; 
+            console.log('done');
+        }else{
+            console.log('error');
+            i--;
+        }
+    
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel () {
+    if (personalMoviesDB.count < 10) {
+        console.log('too moderate quantity of films');
+    } else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30){
+        console.log ('You are a classic viewer');
+    } else if (personalMoviesDB.count >= 30) {
+        console.log ('U are a big fun of movies');
+    } else{
+        console.log('error');
+    }
+}
+detectPersonalLevel ();
+
+/* 2) Create a function showMyDB, will check the privat property. If standing in position
+false - displays the main program object to the console */
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMoviesDB);
+    }
+}
+
+showMyDB(personalMoviesDB.privat);
+
+
+/* 3) Create a writeYourGenres function, in which the user will answer the question 3 times
+"Your favorite genre is $ {sequential number}". Each answer is written to a data array
+genres */
+
+function writeYourGenders () {
+    
+    for (let i = 1; i <= 3; i++) {
+        //const answer = prompt(`your number ${i} favourite gender of film is `);
+        //personalMoviesDB.genders[i - 1] = answer;
+        
+        personalMoviesDB.genders[i - 1] = prompt(`your number ${i} favourite gender of film is `);
+    }
+}
+
+writeYourGenders();
