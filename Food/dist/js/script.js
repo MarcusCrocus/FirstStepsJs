c846834fffa206787bb63eed940a35a49ec05bba
Marcus <crocushtml@gmail.com>
2021-06-14 18:20:30 +0200

    modal windows

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -97,6 +97,7 @@
 
 
 window.addEventListener('DOMContentLoaded', () => {
+  //todo TABS task
   const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -141,6 +142,77 @@ window.addEventListener('DOMContentLoaded', () => {
         }
       });
     }
+  }); //Todo TIMER task
+
+  const deadLine = '2021-06-15'; //* разница между deadLine и текущей датой
+
+  function getTimeRemaining(endTime) {
+    const t = Date.parse(endTime) - Date.parse(new Date()),
+          days = Math.floor(t / (1000 * 60 * 60 * 24)),
+          // общее кол-во милисикунд / (милисекунд в сутках) = сколько суток осталось до текущей даты math.округление
+    hours = Math.floor(t / (1000 * 60 * 60) % 24),
+          // общее кол-во милисикунд / (милисекунд в часе)
+    minutes = Math.floor(t / 1000 / 60 % 60),
+          seconds = Math.floor(t / 1000 % 60);
+    return {
+      // https://alligator.io/js/object-property-shorthand-es6/
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endTime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timeInterval = setInterval(updateClock, 1000);
+    updateClock(); // убирает баг (default date set in HTML) мигания при обновлении страницы
+
+    function updateClock() {
+      const t = getTimeRemaining(endTime); // расчет оставшегося времени
+
+      /* days.innerHTML = t.days;
+         hours.innerHTML = t.hours;
+         minutes.innerHTML = t.minutes;
+         seconds.innerHTML = t.seconds; */
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('.timer', deadLine); //todo Modal window
+  //? добавляем attribute "data-modal" к "связаться с нами" & data-close
+
+  const modalTrigger = document.querySelector('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+  modalTrigger.addEventListener('click', () => {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+  });
+  modalCloseBtn.addEventListener('click', () => {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
   });
 });
 
