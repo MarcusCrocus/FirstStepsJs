0cfc4913a69b14f73c090a36b95c2dad729d4bbd
Marcus <crocushtml@gmail.com>
2021-06-15 20:34:34 +0200

    48 шаблонизация карточек

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -262,8 +262,8 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }
   }); //todo modal появляется когда пользователь долистал стр до конца либо чере опред промежуток времени
-
-  const modalTimerID = setTimeout(openModal, 5000); // todo если пользователь долистал страницу до конца то выскочит модалка
+  //const modalTimerID = setTimeout(openModal, 5000);
+  // todo если пользователь долистал страницу до конца то выскочит модалка
 
   function showModalByScroll() {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -278,6 +278,52 @@ window.addEventListener('DOMContentLoaded', () => {
           openModal();
       }
   },  {once: true} ); */
+  //todo использование классов для карточек
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changeToUAH();
+    } //* потом идут методы
+
+
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    } //* создание структуры <div class="menu__item">
+
+
+    render() {
+      const element = document.createElement('div');
+      element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+      this.parent.append(element);
+    }
+
+  }
+  /* const div = new MenuCard();
+     div.render(); */
+
+
+  new MenuCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес" ', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 9, '.menu .container').render(); // альтернативная запись при одноразовом использовании
+
+  new MenuCard("img/tabs/elite.jpg", "elite", 'Меню “Премиум” ', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 14, '.menu .container').render();
+  new MenuCard("img/tabs/post.jpg", "post", 'Меню "Постное" ', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 21, '.menu .container').render();
 });
 
 /***/ })
