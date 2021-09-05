aa7bc92ccb1fd4576893ef4cd396293fe81dd96b
Marcus <crocus.html@gmail.com>
2021-09-05 15:32:43 +0200

    lesson 61 slick slide advance level

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -2847,56 +2847,128 @@ window.addEventListener('DOMContentLoaded', () => {
     }, 4000);
   } //TODO lesson 61 slider #1
 
+  /* 
+      const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current');
+  
+      let slideIndex = 1; //индекс который определяет текущее положение в слайдере (let потомучто будет изменяться)
+  
+      showSlides(slideIndex); //! что бы все заработало не зыбываем про инициализацию
+  
+      if (slides.length < 10) {
+          total.textContent = `0${slides.length}`;
+      }else {
+          total.textContent = slides.length;
+      }
+  
+      function showSlides(n) {
+          if (n > slides.length) { // если ушли в правую границу то, 
+              slideIndex = 1; //при клике возвращаемся к первому слайду
+          }
+          if (n < 1) { // если в отрицательную сторону то, 
+              slideIndex = slides.length; // возвращаем к последнему имеющемуся
+          }
+  
+          //!! можно сделать с использованием классов
+          slides.forEach(item => item.style.display = 'none'); //скрываем все слайды
+  
+          slides[slideIndex - 1].style.display = 'block'; // показываем нужный
+  
+          // текущий слайд н/п
+          if (slides.length < 10) {
+              current.textContent = `0${slideIndex}`;
+          }else {
+              current.textContent = slideIndex;
+          }
+      
+  
+      }
+      //* функционал по изменению индекса слайда при их перелистывании 
+      function pulusSlides(n) {
+          showSlides(slideIndex += n);
+      }
+  
+      prev.addEventListener('click', () => {
+          pulusSlides(-1);
+      });
+  
+      next.addEventListener('click', () => {
+          pulusSlides(1);
+      });
+   */
+  //!! lesson 62 slick slide #2 advance level
 
+
+  let offset = 0;
+  let slideIndex = 1;
   const slides = document.querySelectorAll('.offer__slide'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
-        current = document.querySelector('#current');
-  let slideIndex = 1; //индекс который определяет текущее положение в слайдере (let потомучто будет изменяться)
-
-  showSlides(slideIndex); //! что бы все заработало не зыбываем про инициализацию
+        current = document.querySelector('#current'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        width = window.getComputedStyle(slidesWrapper).width,
+        slidesField = document.querySelector('.offer__slider-inner');
 
   if (slides.length < 10) {
     total.textContent = `0${slides.length}`;
+    current.textContent = `0${slideIndex}`;
   } else {
     total.textContent = slides.length;
+    current.textContent = slideIndex;
   }
 
-  function showSlides(n) {
-    if (n > slides.length) {
-      // если ушли в правую границу то, 
-      slideIndex = 1; //при клике возвращаемся к первому слайду
+  slidesField.style.width = 100 * slides.length + '%';
+  slidesField.style.display = 'flex';
+  slidesField.style.transition = '0.5s all';
+  slidesWrapper.style.overflow = 'hidden';
+  slides.forEach(slide => {
+    slide.style.width = width;
+  });
+  next.addEventListener('click', () => {
+    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += +width.slice(0, width.length - 2);
     }
 
-    if (n < 1) {
-      // если в отрицательную сторону то, 
-      slideIndex = slides.length; // возвращаем к последнему имеющемуся
-    } //!! можно сделать с использованием классов
+    slidesField.style.transform = `translateX(-${offset}px)`;
 
-
-    slides.forEach(item => item.style.display = 'none'); //скрываем все слайды
-
-    slides[slideIndex - 1].style.display = 'block'; // показываем нужный
-    // текущий слайд н/п
+    if (slideIndex == slides.length) {
+      slideIndex = 1;
+    } else {
+      slideIndex++;
+    }
 
     if (slides.length < 10) {
       current.textContent = `0${slideIndex}`;
     } else {
       current.textContent = slideIndex;
     }
-  } //* функционал по изменению индекса слайда при их перелистывании 
-
-
-  function pulusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-
-  prev.addEventListener('click', () => {
-    pulusSlides(-1);
   });
-  next.addEventListener('click', () => {
-    pulusSlides(1);
+  prev.addEventListener('click', () => {
+    if (offset == 0) {
+      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+    } else {
+      offset -= +width.slice(0, width.length - 2);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
   });
 });
 
